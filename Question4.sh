666d5dd6887dad72f5a077864ed4686fe2720aca
GOPAL THAKUR <gopalk557@gmail.com>
2022-01-19 10:01:28 +0530

    Find a factorial number

--- Question4.sh
+++ Question4.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -x
 
-echo "Enter the number"
-read N
-
-I=$N
-J=2
-
-until [ $J == $N ]
+echo enter m and n
+read m n
+for a in $(seq $m $n)
 do
-if [ `expr $I % $J` == 0 ]
-then
-echo "The number is not prime"
-exit
-else
-J=`expr $J + 1`
-fi
-
+    k=0
+    for i in $(seq 2 $(expr $a - 1))
+    do 
+        if [ $(expr $a % $i) -eq 0 ]
+        then
+            k=1
+            break
+        fi
+    done
+    if [ $k -eq 0 ]
+    then
+    echo $a
+    fi
 done
-echo "The number is not prime"
